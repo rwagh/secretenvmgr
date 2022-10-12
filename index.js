88046fbd82bf7fd506689ca7bda41c6c6efd75e8
Ravindra Wagh <rwagh@customtravelsolutions.com>
2022-10-12 17:42:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+//const decodedBinarySecret;
+AWS.config.credentials = {
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+};
+// Create a Secrets Manager client
+let client = new AWS.SecretsManager({
+  region: process.env.REGION,
+});
+export default {
+  load: async () => {
+    let data = await client
+      .getSecretValue({
+        SecretId: process.env.SECRET_NAME,
+      })
+      .promise();
+
+    let envvars = JSON.parse(data.SecretString);
+    console.log(envvars);
+    let keys = Object.keys(envvars);
+    keys.forEach((k) => {
+      process.env[k] = envvars[k];
+    });
+  },
+};
